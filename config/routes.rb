3020a8af25a220dfdc60b0495bc4aea822f82e63
Maria Coelho <mariiacoelho25@hotmail.com>
2023-08-15 17:13:51 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tasks
+  post 'tasks', to: 'tasks#create'
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete_task'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
